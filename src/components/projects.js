8d873293281a4eb29006a434b08e680e5c7050f1
Saurabh Anand <saurabh789789@gmail.com>
2020-10-19 13:47:29 -0400

    added minor UI changes

--- src/components/projects.js
+++ src/components/projects.js
@@ -9,7 +9,7 @@ class Projects extends Component {
   }
 
   routeToPJ1(){
-  window.open('https://github.com/anand-saurabh/Movie-Review-Service'); //This will open Google in a new
+  window.open('https://github.com/anand-saurabh/Movie-Review-UI-Dashboard--JavaScript-Vue-Bootstrap'); //This will open Google in a new
     }
 
     routeToPJ2(){
@@ -27,57 +27,33 @@ class Projects extends Component {
         <div class="top-level-content">
         <div class="row1">
         <div className="projects-grid">
-          {/* Project 1 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >Movie Review Dashboard</CardTitle>
-            <CardText>
-            This is a movie review dashboard which lets you create, edit, delete or view reviews of different movies based on your role such as critics etc.
-            <br></br>
-            <br></br>
 
-            <span class="label success">Vue</span>
-            &nbsp;
-            <span class="label info">JavaScript</span>
-            &nbsp;
-            <span class="label danger">Bootstrap</span>
-            &nbsp;
-            <span class="label warning">HTML</span>
-            <br></br>
-            <br></br>
-            <span class="label info">CSS</span>
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <CardTitle><b>Cassandra SSTable Generation Utility</b></CardTitle>
+          <CardText style={{'text-align': 'justify'}}>
+          Implemented a utility to generate SSTables from a existing Cassandra table or a .CSV file. This utility can be used to bulk import millions of data into Cassandra tables directly.
+          <br></br>
+          <br></br>
 
-            </CardText>
-            <CardActions border>
-              <Button onClick={()=>this.routeToPJ1()} colored>GitHub</Button>
-              </CardActions>
-          </Card>
-
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >Cassandra SSTable Generation Utility</CardTitle>
-            <CardText>
-            Implemented a utility to generate SSTables from a existing Cassandra table or a .csv file. The application can be used to bulk import the data into Cassandra tables.
-            <br></br>
-            <br></br>
-
-            <span class="label success">Cassandra</span>
-            &nbsp;
-            <span class="label info">Java</span>
-            &nbsp;
-            <span class="label danger">Spring</span>
-            &nbsp;
-            <span class="label warning">Spring Batch</span>
-            <br></br>
-            <br></br>
-            <span class="label info">SSTables</span>
-            </CardText>
-            <CardActions border>
-              <Button onClick={()=>this.routeToPJ2()} colored>GitHub</Button>
-              </CardActions>
-          </Card>
+          <span class="label success">Cassandra</span>
+          &nbsp;
+          <span class="label info">Java</span>
+          &nbsp;
+          <span class="label danger">Spring</span>
+          &nbsp;
+          <span class="label warning">Spring Batch</span>
+          <br></br>
+          <br></br>
+          <span class="label info">SSTables</span>
+          </CardText>
+          <CardActions border>
+            <Button onClick={()=>this.routeToPJ2()} colored>GitHub</Button>
+            </CardActions>
+        </Card>
 
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >Application To Track User GitHub Issues</CardTitle>
-            <CardText>
+            <CardTitle><b>Application To Track User GitHub Issues</b></CardTitle>
+            <CardText style={{'text-align': 'justify'}}>
             Developed REST APIs to talk to GitHub client code and get data for a user based on user's GitHub Ids. The data include number of open issues, issues opened more than 7 days ago.
             <br></br>
             <br></br>
@@ -98,6 +74,31 @@ class Projects extends Component {
               <Button onClick={()=>this.routeToPJ3()} colored>GitHub</Button>
               </CardActions>
           </Card>
+
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle><b>Movie Review Dashboard with CRUD Operations</b></CardTitle>
+            <CardText style={{'text-align': 'justify'}}>
+            The movie review dashboard project lets you create, edit, delete or view reviews of movies based on the user roles such as critics. Also, we can search a movie by applying filters in UI.
+            <br></br>
+            <br></br>
+
+            <span class="label success">Vue</span>
+            &nbsp;
+            <span class="label info">JavaScript</span>
+            &nbsp;
+            <span class="label danger">Bootstrap</span>
+            &nbsp;
+            <span class="label warning">HTML</span>
+            <br></br>
+            <br></br>
+            <span class="label info">CSS</span>
+
+            </CardText>
+            <CardActions border>
+              <Button onClick={()=>this.routeToPJ1()} colored>GitHub</Button>
+              </CardActions>
+          </Card>
+
           </div>
           </div>
           <br></br>
@@ -107,9 +108,9 @@ class Projects extends Component {
           <div className="projects-grid">
             {/* Project 1 */}
             <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-              <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >BitTorrent Application</CardTitle>
-              <CardText>
-              This a implementation of BitTorrent application using socket programming in Java.
+              <CardTitle><b>BitTorrent Application: Distributed and Scalable System Implementation</b></CardTitle>
+              <CardText style={{'text-align': 'justify'}}>
+              This a implementation of BitTorrent application using socket programming in Java. This uses decentralized P2P communication protocol for sharing data and files.
               <br></br>
               <br></br>
 
@@ -130,9 +131,9 @@ class Projects extends Component {
             </Card>
 
             <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-              <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >Application Deployment using Jenkins, Ansible, Tomcat</CardTitle>
-              <CardText>
-              This project shows step by step process to deploy a Maven or Gradle Application in Cloud using Jenkins, Tomcat and Ansible Scripts.
+              <CardTitle><b>Application Deployment using Jenkins, Ansible, Tomcat</b></CardTitle>
+              <CardText style={{'text-align': 'justify'}}>
+              This project shows step by step process to deploy a Maven or Gradle Application in the Cloud (AWS etc) using Jenkins, Tomcat and Ansible Scripts.
               <br></br>
               <br></br>
 
@@ -153,8 +154,8 @@ class Projects extends Component {
             </Card>
 
             <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-              <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >Most Frequent Hashtags on Social Media</CardTitle>
-              <CardText>
+              <CardTitle><b>Most Frequent Hashtags on Social Media</b></CardTitle>
+              <CardText style={{'text-align': 'justify'}}>
               This application finds the most frequent hashtags on Social Media like Twitter,Facebook. This application is capable of processing millions of tags in less than a sec.
               <br></br>
               <br></br>
@@ -179,9 +180,9 @@ class Projects extends Component {
             <div class="row3">
             <div className="projects-grid">
               <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >Distributed Messaging Application using RabbitMQ and Golang</CardTitle>
-                <CardText>
-                This is messaging application built using RabbitMQ as the message-broker. I implemented this using Golang language.
+                <CardTitle ><b>Distributed Messaging Application using RabbitMQ and Golang</b></CardTitle>
+                <CardText style={{'text-align': 'justify'}}>
+                This is messaging application built using RabbitMQ as the message-broker. I made use of Golang language for the implementation.
                 <br></br>
                 <br></br>
 
@@ -200,8 +201,9 @@ class Projects extends Component {
                   </CardActions>
               </Card>
                 <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                  <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >Blockchain Bidding Application using Solidity</CardTitle>
-                  <CardText> Implemented a smart contract using Solidity language for a Blockchain bidding application
+                  <CardTitle><b>Distributed System: Blockchain Bidding Application using Solidity</b></CardTitle>
+                  <CardText style={{'text-align': 'justify'}}>
+                  Implemented smart contracts using solidity language for a Blockchain Bidding application. Scaled the system to support multiple users.
                   <br></br>
                   <br></br>
 
@@ -220,8 +222,8 @@ class Projects extends Component {
                     </CardActions>
                 </Card>
                 <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                  <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >Merger and Acquistion using Blockchain</CardTitle>
-                  <CardText>
+                  <CardTitle><b>Distributed System: Merger and Acquistion using Blockchain</b></CardTitle>
+                  <CardText style={{'text-align': 'justify'}}>
                   This project is an implementation of the merger and acquisition process on a Blockchain Network using Solidity.
                   <br></br>
                   <br></br>
